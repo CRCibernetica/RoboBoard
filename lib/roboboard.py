35f7a24ba350cea737f665be5e97cccacf02dbe6
Bentley Born <bentley@crcibernetica.com>
2022-07-02 14:50:18 -0700

    added servo functions

--- lib/roboboard.py
+++ lib/roboboard.py
@@ -2,44 +2,44 @@ from machine import Pin, PWM
 from neopixel import NeoPixel
 
 class RoboBoard:
-    def __init__(self):
-        self.pixel_pin = Pin(2, Pin.OUT)
-        self.np = NeoPixel(self.pixel_pin, 1)
-        self.np[0] = (0,0,0)
-        self.m1a = PWM(Pin(12))
-        self.m1b = PWM(Pin(14))
-        self.m2a = PWM(Pin(13))
-        self.m2b = PWM(Pin(15))
+    def __init__(self, m1a=12, m1b=14, m2a=13, m2b=15):
+        self._pixel_pin = Pin(2, Pin.OUT)
+        self._np = NeoPixel(self._pixel_pin, 1)
+        self._np[0] = (0,0,0)
+        self._m1a = PWM(Pin(m1a))
+        self._m1b = PWM(Pin(m1b))
+        self._m2a = PWM(Pin(m2a))
+        self._m2b = PWM(Pin(m2b))
     
     def pixel(self, color=(0,0,0)):
-        self.np[0] = color
-        self.np.write()
+        self._np[0] = color
+        self._np.write()
     
     def arcoiris(self, n=0):
-        color = self.wheel(n)
+        color = self._wheel(n)
         self.pixel(color)
     
     def motor_1(self, vel=0):
         if vel < -1023 | vel > 1023:
             print(f"velocidad no valida: {vel}")
         if vel >= 0:
-            self.m1a.duty(vel)
-            self.m1b.duty(0)
+            self._m1a.duty(vel)
+            self._m1b.duty(0)
         if vel < 0:
-            self.m1a.duty(0)
-            self.m1b.duty(vel * -1)
+            self._m1a.duty(0)
+            self._m1b.duty(vel * -1)
     
     def motor_2(self, vel=0):
         if vel < -1023 | vel > 1023:
             print(f"velocidad no valida: {vel}")
         if vel >= 0:
-            self.m2a.duty(vel)
-            self.m2b.duty(0)
+            self._m2a.duty(vel)
+            self._m2b.duty(0)
         if vel < 0:
-            self.m2a.duty(0)
-            self.m2b.duty(vel * -1)
+            self._m2a.duty(0)
+            self._m2b.duty(vel * -1)
     
-    def wheel(self, pos):
+    def _wheel(self, pos):
         # Adafruit color wheel code
         # Input a value 0 to 255 return RGB tuple.
         if pos < 0:
@@ -54,3 +54,44 @@ class RoboBoard:
         else:
             pos -= 170
             return (0, int(pos * 3), int(255 - pos * 3))
+        
+    class Servo:
+        def __init__(self, pin):
+            """
+            Initialize the servo
+
+            """
+            self._servo = PWM(Pin(pin))
+            self._servo.freq(50) # 50Hz for most hobby servos
+            
+            # Calibrate the servo
+            self._duty_eq = self.calibrate(min_duty = 51, max_duty = 102)
+            self.angle(0)
+        
+        def calibrate(self, min_duty, max_duty, min_angle = 0, max_angle = 180):
+            """
+            Calibrate the servo range and angle
+            
+            """
+            
+            slope = (min_duty-max_duty)/(min_angle - max_angle)
+            y_intercept = min_duty - slope * (min_angle)
+            duty_calc = lambda angle: slope * angle + y_intercept
+            
+            return duty_calc
+
+        def angle(self, angle):
+            """
+            Set the angle of the servo.
+            
+            """
+
+            duty = int(self._duty_eq(angle))
+            self._servo.duty(duty)
+        
+        def set_duty(self, duty):
+            """
+            Manually set the duty cycle of the signal.
+            
+            """
+            self._servo.duty(duty)
--- main.py
+++ main.py
@@ -1,29 +1,6 @@
 from roboboard import RoboBoard
 from time import sleep_ms
 
-# crear el objeto
 rb = RoboBoard()
 
-ROJO = (255,0,0)
-VERDE = (0,255,0)
-AZUL = (0,0,255)
-
-# pixel utiliza tuples de RGB
-rb.pixel(AZUL)
-sleep_ms(1000)
-rb.pixel(ROJO)
-sleep_ms(1000)
-rb.pixel((0,255,0))
-sleep_ms(1000)
-
-# arcoiris es una rueda de colores de 0-255 (empieza en verde termina en verde)
-rb.arcoiris(125)
-sleep_ms(1000)
-
-for i in range(255):
-    rb.arcoiris(i)
-    sleep_ms(10)
-
-# los motores reciben velocidades de -1023 a 1023 (detras - adelante)
-rb.motor_1(1023)
-rb.motor_2(-1023)
+rb.motor_1(0)
